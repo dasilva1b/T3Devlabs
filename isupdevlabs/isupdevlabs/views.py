e6f83af6bc46ce04e48aff4e17979c97fd8e5c99
Carlos Da Silva <cdssox18@gmail.com>
2013-11-28 22:53:57 -0430

    Version Inicial de la aplicacion similar a ISUP. T3 devlab.

---
+++ isupdevlabs/isupdevlabs/views.py
@@ -0,0 +1,41 @@
+"""Views donde estan las funciones que procesan la solicitud del usuario
+Autor: Carlos Da Silva(10-10175)
+
+"""
+
+
+
+from django.http import HttpResponse
+import datetime
+from django.template import Context
+from django.template.loader import get_template
+from django.shortcuts import render
+import urllib2
+
+
+def home(request):
+  """Simplemente retorna el request para abrir la pagina de inicio o index.
+  
+  """
+  return render (request,'index.html')
+  
+def consultar_pagina(request):
+  """Dada una pagina y el protocolo, revisa si dicha pagina esta funcionante
+     a traves del uso de las funciones del modulo urllib2 y la captura de
+     excepcines   
+     
+  """
+  #Obtenemos lo solicitado en el formulario
+  pagina=request.POST['pagina'];
+  protocolo=request.POST['protocol'];
+  
+  #Le agregamos el protocolo
+  pagina=protocolo+pagina;
+    
+  try:
+    estado=urllib2.urlopen(pagina,timeout=8).getcode()
+    if (estado == 200):
+      return render (request, 'exitopagina.html')
+  except IOError:
+    return render (request, 'error.html')
+    
